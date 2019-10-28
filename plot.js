dc42e8aac926c9eac687de770dd73d99d628430e
Kateka Seth <katekaseth@gmail.com>
2019-10-27 22:58:12 -0700

    fix generation filter

--- plot.js
+++ plot.js
@@ -1,7 +1,8 @@
 "use-strict";
 
-let data;
-let plotContainer ;
+let allData;
+let filteredData;
+let plotContainer;
 let legendContainer;
 const msm = {
     width: 1000,
@@ -42,61 +43,62 @@ window.onload = function () {
         .attr('height', msm.height);
     // d3.csv is basically fetch but it can be be passed a csv file as a parameter
     d3.csv("pokemon.csv")
-        .then((d) => makeScatterPlot(d))
+        .then((d) => main(d))
 }
 
-function makeScatterPlot(d) {
-    data = d;
+function main(d) {
+    allData = d;
+    makeScatterPlot(d);
+    addTypeLegend();
+    makeGenerationFilter();
+}
+
+function makeGenerationFilter() {
+    let dropDown = d3.select("#filter").append("select")
+        .attr("name", "generation");
+
+    let distinctGens = [...new Set(allData.map(d => d["Generation"]))];
+    distinctGens.push("All")
+    let defaultGen = "All";
+
+    let options = dropDown.selectAll("option")
+        .data(distinctGens)
+        .enter()
+        .append("option")
+        .text(function (d) {
+            return d;
+        })
+        .attr("value", function (d) {
+            return d;
+        })
+        .attr("selected", function (d) {
+            return d === defaultGen
+        })
+
+    // showCircles(dropDown.node()); //this will filter initially
+    dropDown.on("change", function () {
+        // showCircles(this)
+        let selected = this.value;
+        if (selected === "All") {
+            filteredData = allData;
+        } else {
+            filteredData = allData.filter((row) => {
+                return row["Generation"] === selected
+            })
+        }
+        plotContainer.selectAll("*").remove()
+        makeScatterPlot(filteredData)
+    });
+}
+
+function makeScatterPlot(data) {
     let spDef = data.map((row) => parseInt(row["Sp. Def"]));
     let total = data.map((row) => parseInt(row["Total"]));
     let axesMinMax = findMinMax(spDef, total);
     let scalers = getScalers(axesMinMax);
     drawAxes(scalers);
     drawAxesLabels();
-    plotData(scalers);
-    addTypeLegend();
-
-    let dropDown = d3.select("#filter").append("select")
-        .attr("name", "generation");
-
-    let distinctGens = [...new Set(data.map(d => d["Generation"]))];
-    //distinctGens.add("All")
-    distinctGens.push("All")
-    let defaultGen = "All";
-
-    let options = dropDown.selectAll("option")
-           .data(distinctGens)
-           .enter()
-           .append("option")
-           .text(function (d) { return d; })
-           .attr("value", function (d) { return d; })
-           .attr("selected", function(d){ return d === defaultGen })
-           
-    showCircles(dropDown.node());//this will filter initially
-    dropDown.on("change", function() {
-        showCircles(this)
-    });
-}
-
-function showCircles(me) {
-    let selected = me.value;
-    if (selected === "All") {
-        displayOthers = "inline";
-        display = "inline";
-    } else {
-        displayOthers = me.checked ? "inline" : "none";
-        display = me.checked ? "none" : "inline";
-    }
-    
-    plotContainer.selectAll(".circles")
-        .data(data)
-        .filter(function(d) {return selected !== d["Generation"];})
-        .attr("display", displayOthers);
-        
-    plotContainer.selectAll(".circles")
-        .data(data)
-        .filter(function(d) {return selected === d["Generation"];})
-        .attr("display", display);
+    plotData(scalers, data);
 }
 
 function addTypeLegend() {
@@ -107,10 +109,14 @@ function addTypeLegend() {
         .enter()
         .append("rect")
         .attr("x", 10)
-        .attr("y", function (d, i) { return 130 + (i * 30)})
+        .attr("y", function (d, i) {
+            return 130 + (i * 30)
+        })
         .attr("width", 20)
         .attr("height", 20)
-        .attr("fill", function (d) { return d })
+        .attr("fill", function (d) {
+            return d
+        })
     let typeArray = Object.keys(colors)
     legendContainer.append('g')
         .selectAll("text")
@@ -118,30 +124,34 @@ function addTypeLegend() {
         .enter()
         .append("text")
         .attr("x", 35)
-        .attr("y", function (d, i) { return 145 + (i * 30)})
-        .text(function (d) { return d })
+        .attr("y", function (d, i) {
+            return 145 + (i * 30)
+        })
+        .text(function (d) {
+            return d
+        })
 }
 
-function plotData(scalers) {
+function plotData(scalers, data) {
     // make tooltip
     let tooltipDiv = d3.select("body").append("div")
         .attr("class", "tooltip")
         .style("opacity", 0);
-    
+
     const mouseOverFunc = selection =>
         selection.transition()
-            .duration(300)
-            .style("opacity", 1)
-            .attr("r", 16);
-    
+        .duration(300)
+        .style("opacity", 1)
+        .attr("r", 16);
+
     const mouseOutFunc = selection =>
         selection.transition()
-            .duration(500)
-            .style("opacity", .8)
-            .attr("r", 10)
+        .duration(500)
+        .style("opacity", .8)
+        .attr("r", 10)
 
-    d3.selection.prototype.moveToFront = function() {
-        return this.each(function(){
+    d3.selection.prototype.moveToFront = function () {
+        return this.each(function () {
             this.parentNode.appendChild(this);
         });
     };
@@ -152,12 +162,18 @@ function plotData(scalers) {
         .data(data)
         .enter()
         .append("circle")
-        .attr("cx", function (d) { return scalers.xScale(d["Sp. Def"]); } )
-        .attr("cy", function (d) { return scalers.yScale(d["Total"]); } )
+        .attr("cx", function (d) {
+            return scalers.xScale(d["Sp. Def"]);
+        })
+        .attr("cy", function (d) {
+            return scalers.yScale(d["Total"]);
+        })
         .attr("r", 10)
         .attr('stroke', "black")
         .attr('stroke-width', 1)
-        .attr('fill', function (d) { return colors[d["Type 1"]]})
+        .attr('fill', function (d) {
+            return colors[d["Type 1"]]
+        })
         .attr("class", "circles")
         .style('opacity', .8)
         .on("mouseover", (d, i, nodes) => {
@@ -167,9 +183,9 @@ function plotData(scalers) {
             tooltipDiv.transition()
                 .duration(200)
                 .style("opacity", 1)
-             tooltipDiv.html("<b>" + d["Name"] + "</b> <br/>" +
-                        d["Type 1"] + "<br/>" +
-                        d["Type 2"] + "<br/>")
+            tooltipDiv.html("<b>" + d["Name"] + "</b> <br/>" +
+                    d["Type 1"] + "<br/>" +
+                    d["Type 2"] + "<br/>")
                 .style("left", (d3.event.pageX + 5) + "px")
                 .style("top", (d3.event.pageY - 10) + "px");
         })
@@ -199,7 +215,7 @@ function getScalers(axesMinMax) {
         .domain([axesMinMax.xMin - 10, axesMinMax.xMax + 10])
         .range([msm.marginAll, msm.width - msm.marginAll]);
 
-        // function to scale y
+    // function to scale y
     let yScale = d3.scaleLinear()
         .domain([axesMinMax.yMax + 50, axesMinMax.yMin - 50]) // give domain buffer
         .range([msm.marginAll, msm.height - msm.marginAll]);
