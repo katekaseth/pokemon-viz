b670a844e9895f2f16b7ccd7e705c9254d450dce
Kateka Seth <katekaseth@gmail.com>
2019-10-29 00:06:26 -0700

    show overlapping pokemons

--- plot.js
+++ plot.js
@@ -8,6 +8,8 @@ let plotContainer;
 let legendContainer;
 let tooltipDiv;
 let scalers;
+let sameStats;
+
 const msm = {
     width: 1000,
     height: 800,
@@ -74,6 +76,7 @@ function main(d) {
     makeLegendaryFilter();
 }
 
+// adds radio buttons to filter for legendary pokemon
 function makeLegendaryFilter() {
     d3.selectAll("input").on("change", function () {
         legendSelection = this.value;
@@ -90,13 +93,12 @@ function makeLegendaryFilter() {
                 return row["Legendary"] === legendSelection
             });
         }
-        // plotContainer.selectAll("*").remove()
-        // makeScatterPlot(filteredData)
         transitionDots(filteredData);
         addTypeLegend(filteredData);
     });
 }
 
+// adds a drop down to filter by generation
 function makeGenerationFilter() {
     let dropDown = d3.select("#filter").append("select")
         .attr("name", "generation");
@@ -119,7 +121,6 @@ function makeGenerationFilter() {
         })
 
     dropDown.on("change", function () {
-        let oldLength = filteredData.length;
         genSelection = this.value;
         let toBeFiltered = allData;
         if (legendSelection !== "All") {
@@ -134,14 +135,14 @@ function makeGenerationFilter() {
                 return row["Generation"] === genSelection
             });
         }
-        //plotContainer.selectAll("*").remove();
-        //makeScatterPlot(filteredData);
         transitionDots(filteredData);
         addTypeLegend(filteredData);
     });
 }
 
+// function to make a nice transition when filtering dots
 function transitionDots(filteredData) {
+    sameStats = getSameStatPokemons(filteredData)
     let circles = plotContainer.selectAll("circle")
         .data(filteredData)
 
@@ -172,8 +173,6 @@ function transitionDots(filteredData) {
             return colors[d["Type 1"]]
         })
         .style('opacity', .8)
-        
-        
 
     // get rid of circles that aren't in the data anymore
     circles.exit()
@@ -183,6 +182,7 @@ function transitionDots(filteredData) {
         .remove()
 }
 
+// draws the scatterplot
 function makeScatterPlot(data) {
     let spDef = data.map((row) => parseInt(row["Sp. Def"]));
     let total = data.map((row) => parseInt(row["Total"]));
@@ -193,9 +193,11 @@ function makeScatterPlot(data) {
     plotData(scalers, data);
 }
 
+// add the type 1 legend bar
 function addTypeLegend(data) {
     const typeArray = Object.keys(colors)
     legendContainer.selectAll("*").remove()
+    // filters for only the types currently on the scatterplot
     let distinctType1 = [...new Set(data.map(d => d["Type 1"]))];
     let intersection = typeArray.filter(x => distinctType1.includes(x))
     legendContainer.append('g')
@@ -211,6 +213,7 @@ function addTypeLegend(data) {
             return d
         })
 
+    // filters only the color currently on the scatterplot
     let colorInter = [];
     intersection.forEach(function (element) {
         colorInter.push(colors[element])
@@ -230,19 +233,21 @@ function addTypeLegend(data) {
             return d
         })
 
+    // bar title
     legendContainer.append("text")
         .attr("x", 10)
         .attr("y", 110)
         .text("Type 1")
 }
 
+// plot all of the dots
 function plotData(scalers, data) {
     d3.selection.prototype.moveToFront = function () {
         return this.each(function () {
             this.parentNode.appendChild(this);
         });
     };
-
+    sameStats = getSameStatPokemons(data)
     // add dots
     plotContainer.append('g')
         .selectAll("dot")
@@ -271,6 +276,8 @@ function plotData(scalers, data) {
         });
 }
 
+// function to be called on mouseout, 
+// fades out the tooltip and unexpand selected dot
 function dotMouseOut(i, nodes) {
     tooltipDiv.transition()
         .duration(500)
@@ -279,18 +286,42 @@ function dotMouseOut(i, nodes) {
         .call(mouseOutFunc);
 }
 
+// function to be called on mouseover,
+// fades in tooltip and expands selected dot
 function dotMouseOver(d, i, nodes) {
+    let htmlText = getTooltipText(d);
     d3.select(nodes[i])
         .call(mouseOverFunc);
     d3.select(nodes[i]).moveToFront();
     tooltipDiv.transition()
         .duration(200)
         .style("opacity", 1)
-    tooltipDiv.html("<b>" + d["Name"] + "</b> <br/>" +
-            d["Type 1"] + "<br/>" +
-            d["Type 2"] + "<br/>")
+    tooltipDiv.html(htmlText)
         .style("left", (d3.event.pageX + 5) + "px")
         .style("top", (d3.event.pageY - 10) + "px");
+}
+
+// returns the text to be displayed on the tooltip
+// shows all pokemon names if there are multiple with the same stat
+// shows type if type all matches, otherwise *
+function getTooltipText(d) {
+    let samePokemon = searchArray(sameStats, d["Name"]);
+    let htmlText = "<b>" + d["Name"] + "</b> <br/>" +
+        d["Type 1"] + "<br/>" +
+        d["Type 2"] + "<br/>";
+    if (samePokemon) {
+        htmlText = "<b>" + [...samePokemon].join(", ") + "</b> <br/>"
+        let type1 = new Set();
+        let type2 = new Set();
+        samePokemon.forEach(name => {
+            let pokemon = allData.filter(row => row["Name"] === name)[0]
+            type1.add(pokemon["Type 1"])
+            type2.add(pokemon["Type 2"])
+        })
+        type1.size === 1 ? htmlText += [...type1] + "<br/>" : htmlText += "* <br/>"
+        type2.size === 1 ? htmlText += [...type2] + "<br/>" : htmlText += "* <br/>"
+    }
+    return htmlText;
 }
 
 // find min and max for arrays of x and y
@@ -304,6 +335,7 @@ function findMinMax(x, y) {
     };
 }
 
+// return the an object containing the x and y scaling functions
 function getScalers(axesMinMax) {
     // function to scale x value
     let xScale = d3.scaleLinear()
@@ -335,6 +367,7 @@ function drawAxes(scalers) {
         .call(yAxis);
 }
 
+// draw x-axis label, y-axis label, and title
 function drawAxesLabels() {
     plotContainer.append('text')
         .attr('x', (msm.width - 2 * msm.marginAll) / 2 - 130)
@@ -352,4 +385,33 @@ function drawAxesLabels() {
         .attr('transform', 'translate( 15,' + (msm.height / 2 + 30) + ') rotate(-90)')
         .style('font-size', '10pt')
         .text("Total Stats");
+}
+
+// https://stackoverflow.com/questions/39856883/sum-values-in-object-if-multiple-keys-are-the-same-js
+// returns a 2D array of the pokemon in which each array contains
+// pokemon with the same total and sp. def
+function getSameStatPokemons(data) {
+    let o = {}
+    let result = data.reduce(function (r, e) {
+        let key = e["Total"] + "|" + e["Sp. Def"]
+        if (!o[key]) {
+            o[key] = [e["Name"]];
+            r.push(o[key]);
+        } else {
+            o[key].push(e["Name"]);
+        }
+        return r;
+    }, [])
+    result = result.filter(array => {
+        return array.length != 1
+    })
+    return result;
+}
+
+// given a 2d array to search and the element to find
+// return the row containing the element
+function searchArray(array, find) {
+    for (let i = 0, l = array.length; i < l; i++) {
+        if (array[i][1] === find) return array[i];
+    }
 }
